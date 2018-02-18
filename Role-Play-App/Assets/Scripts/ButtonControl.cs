7f91243f0afd730170a4c87cde60df261ee55db8
HumanoidBeast <mcreijnders@hfg-karlsruhe.de>
2018-02-18 14:31:40 +0100

    Changed Button Event

---
+++ Role-Play-App/Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour {
+
+	public GameObject image;
+
+	public void buttonEvent(){
+		if (image.active)
+			image.SetActive (false);
+		else
+			image.SetActive (true);
+		
+	}
+
+}
