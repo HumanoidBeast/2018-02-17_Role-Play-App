ee61232536629b91080d518f44e65aa35916f74e
Ninjamurai <email@martijn-reijnders.de>
2018-02-18 21:30:44 +0100

    sliders scaled

--- Role-Play-App/Assets/Scripts/PlayerHealth.cs
+++ Role-Play-App/Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour {
 
+	public Slider healthbar;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		healthbar.value = 100;
+
 	}
 	
 	// Update is called once per frame
