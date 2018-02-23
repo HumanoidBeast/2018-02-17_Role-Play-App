3108d34df072b17aab7ea53d8d2fe916e518818b
Ninjamurai <email@martijn-reijnders.de>
2018-02-23 20:09:18 +0100

    added custom network manager

---
+++ Role-Play-App/Assets/Scripts/NetworkManager_custom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NetworkManager_custom : NetworkManager {
+
+	public void startHost(){
+
+		setPort ();
+		NetworkManager.singleton.StartHost ();
+
+	}
+
+	public void joinGame(){
+
+		setIpAddress ();
+		setPort ();
+		NetworkManager.singleton.StartClient ();
+
+	}
+	public void setIpAddress(){
+
+		string IpAddress = GameObject.Find ("InputField").transform.Find("Text").GetComponent<Text> ().text;
+		NetworkManager.singleton.networkAddress = IpAddress;
+
+	}
+
+	public void setPort(){
+
+		NetworkManager.singleton.networkPort = 7777;
+
+	}
+}
