fb26ac7be392d67446c0767e8fb9385d2834c3d9
Davkakra <36601928+Davkakra@users.noreply.github.com>
2018-02-23 22:44:55 +0100

    First CharacterList example

---
+++ Role-Play-App/Assets/Scripts/Char_List.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_List : MonoBehaviour {
+
+	public GameObject Prefab;
+	public Transform Sheet;
+
+
+	GameObject ListObject;
+
+
+
+	void Start(){
+		ListObject = Instantiate (Prefab, Sheet);
+		ListObject = GameObject.Find ("PlayerSheet");
+		ListObject.SetActive (false);
+
+	}
+
+	public void AddList(){
+		ListObject = Instantiate (Prefab, Sheet);
+
+
+	}
+	
+
+}
