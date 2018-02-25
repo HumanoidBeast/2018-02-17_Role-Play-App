3079430fdffd64e3a4d3a051ccf3e6285c1d0188
HumanoidBeast <mcreijnders@hfg-karlsruhe.de>
2018-02-25 17:36:26 +0100

    Added Saving Manager and AbilityBaseClass

--- Role-Play-App/Assets/Scripts/AbilityBuild.cs
+++ Role-Play-App/Assets/Scripts/AbilityBuild.cs
@@ -5,13 +5,28 @@ using UnityEngine.UI;
 
 public class AbilityBuild : MonoBehaviour {
 
-	[Space(5)][Header("Clock")]
-	public GameObject Minute;
-	public GameObject HalfHour;
-	public GameObject ThreeHours;
-	public GameObject Day;
+	[Space(10)][Header("Clock")]
+	public GameObject TimeTier1;
+	public GameObject TimeTier2;
+	public GameObject TimeTier3;
+	public GameObject TimeTier4;
 	public Text durationDisplay;
 	public int Duration = 10;
+	[Space(1)][Header("ClockTier0")]
+	public int tierZeroValue = 10;
+	public string tierZeroText = "A few seconds";
+	[Space(1)][Header("ClockTier1")]
+	public int tierOneValue = 60;
+	public string tierOneText = "One minute";
+	[Space(1)][Header("ClockTier2")]
+	public int tierTwoValue = 1800;
+	public string tierTwoText = "Half an hour";
+	[Space(1)][Header("ClockTier3")]
+	public int tierThreeValue = 9600;
+	public string tierThreeText = "Three Hours";
+	[Space(1)][Header("ClockTier4")]
+	public int tierFourValue = 76800;
+	public string tierFourText = "All day long";
 
 	[Space(2)][Header("Special")]
 	public GameObject Special;
@@ -20,29 +35,29 @@ public class AbilityBuild : MonoBehaviour {
 
 
 	public void ClockSet() {
-		if (Duration == 10) {
-			Minute.SetActive (true);
-			Duration = 60;
-			durationDisplay.text = "One minute";
-		} else if (Duration == 60) {
-			Minute.SetActive (false);
-			HalfHour.SetActive (true);
-			Duration = 1800;
-			durationDisplay.text = "Half an hour";
-		} else if (Duration == 1800) {
-			HalfHour.SetActive (false);
-			ThreeHours.SetActive (true);
-			Duration = 9600;
-			durationDisplay.text = "Three Hours";
-		} else if (Duration == 9600) {
-			ThreeHours.SetActive (false);
-			Day.SetActive (true);
-			Duration = 76800;
-			durationDisplay.text = "All day long";
-		} else if (Duration == 76800) {
-			Day.SetActive (false);
-			Duration = 10;
-			durationDisplay.text = "A few seconds";
+		if (Duration == tierZeroValue) {
+			TimeTier1.SetActive (true);
+			Duration = tierOneValue;
+			durationDisplay.text = tierOneText;
+		} else if (Duration == tierOneValue) {
+			TimeTier1.SetActive (false);
+			TimeTier2.SetActive (true);
+			Duration = tierTwoValue;
+			durationDisplay.text = tierTwoText;
+		} else if (Duration == tierTwoValue) {
+			TimeTier2.SetActive (false);
+			TimeTier3.SetActive (true);
+			Duration = tierThreeValue;
+			durationDisplay.text = tierThreeText;
+		} else if (Duration == tierThreeValue) {
+			TimeTier3.SetActive (false);
+			TimeTier4.SetActive (true);
+			Duration = tierFourValue;
+			durationDisplay.text = tierFourText;
+		} else if (Duration == tierFourValue) {
+			TimeTier4.SetActive (false);
+			Duration = tierZeroValue;
+			durationDisplay.text = tierZeroText;
 		}
 	}
 
---
+++ Role-Play-App/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour {
+
+	public static SaveManager save;
+	// Use this for initialization
+	void Awake () {
+		if (save == null) {
+			DontDestroyOnLoad (gameObject);
+			save = this;
+		} else if (save != this){
+			Destroy (gameObject);
+		}
+	}
+	public void Save(){
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/saveData.dat");
+
+		SaveData data = new SaveData ();
+		//Write all Values to save: data.health = health;
+
+		bf.Serialize (file, data);
+		file.Close ();
+	}	
+	public void Load(){
+		if (File.Exists (Application.persistentDataPath + "/saveData.dat")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/saveData.dat", FileMode.Open);
+			SaveData data = (SaveData)bf.Deserialize (file);
+			file.Close ();
+			//Write all Values to load: health = data.health;
+
+		}	
+	}
+
+}
+[Serializable]
+class SaveData
+{
+		//Write all Datatypes that you wanna be able to Store here: public int health;
+
+}
